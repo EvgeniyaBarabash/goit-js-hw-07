c6f8a8578523f72ae0c2d21df77ddb57ce4dcf09
EvgeniyaBarabash <barabash1989@gmail.com>
2021-08-12 13:06:59 +0300

    добавляет 3 метод счетчика

--- js/task-04.js
+++ js/task-04.js
@@ -1,17 +1,17 @@
 const totalValue = document.querySelector('#value');
 const decrementValue = document.querySelector('[data-action="decrement"]');
 const incrementValue = document.querySelector('[data-action="increment"]');
-decrementValue.addEventListener('click', onDecrementClick);
-incrementValue.addEventListener('click', onIncrementClick);
-let counterValue = 0;
-function onDecrementClick(){
-    counterValue -=1;
-    totalValue.innerHTML=counterValue;
-};
-function onIncrementClick(){
-    counterValue +=1;
-    totalValue.innerHTML=counterValue;
-};
+// decrementValue.addEventListener('click', onDecrementClick);
+// incrementValue.addEventListener('click', onIncrementClick);
+// let counterValue = 0;
+// function onDecrementClick(){
+//     counterValue -=1;
+//     totalValue.innerHTML=counterValue;
+// };
+// function onIncrementClick(){
+//     counterValue +=1;
+//     totalValue.innerHTML=counterValue;
+// };
 
 
 // 2 вариант
@@ -21,3 +21,27 @@ function onIncrementClick(){
 // function onIncrementClick(){
 //     totalValue.textContent =Number(totalValue.textContent) + 1;
 // };
+
+// 3 вариант
+
+function createCounter(){
+    let counterValue = 0;
+    function increment(){
+return counterValue +=1;
+    };
+    function decrement(){
+        return counterValue -=1;
+    };
+    return {increment, decrement};
+};
+const createObj = createCounter()
+incrementValue.addEventListener('click', (e)=>{
+    let value= createObj.increment();
+    // console.log(value);
+    totalValue.textContent = value;
+});
+decrementValue.addEventListener('click', (e)=>{
+    let value = createObj.decrement();
+    // console.log(value);
+    totalValue.textContent = value;
+});
